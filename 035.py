a1bb4d25ebb173c41c6f317046973b992e77477a
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-21 12:37:13 -0300

    035.py

--- 035.py
+++ 035.py
@@ -7,16 +7,30 @@ Quantas mulheres têm menos de 20 anos
 '''
 media = 0
 soma_idades = 0
+mulher_abaixo=0
+idade_homem = 0
+nome_homem = 'Nenhum homem informado'
 for i in range(2):
     nome = input('Favor digitar o nome:')
     idade = int(input('Favor digitar a idade:'))
     soma_idades += idade
-    media = soma_idades/4
-    sexo = input('Favor informar o sexo: ').lower()
+    sexo = input('Favor informar o sexo: (H= Homem, M = Mulher').lower()
+
+    if sexo=="H" and idade > idade_homem:
+        idade_homem=idade
+        nome_homem = nome
+    if sexo =="M" and idade<20:
+        mulher_abaixo+=1
 
 
 
-print(idade)
 
+
+
+media = soma_idades/2
+print(f'a média de idade é {media}')
+
+print(f'o homem mais velho é: {nome_homem}')
+print(f'a quatidade de mulheres abaixo de 20 anos é {mulher_abaixo}')
 
 
