5aa3591a2299ddb58da2d3df112100bd2f5757a0
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 01:10:28 -0300

    Delete extra004.py

--- extra004.py
+++
@@ -1,8 +0,0 @@
-#Gerador de Histórias
-#Solicite ao usuário seu nome, sua cidade e seu animal de estimação, então use essas informações para gerar uma história curta.
-
-nome = input('Favor informar seu nome: ').capitalize()
-cidade=input('Favor informar sua cidade: ').capitalize()
-animal=input('Favor informar seu animal de estimação: ')
-
-print(f'a pessoa {nome}, que mora na {cidade} tem um {animal} que é muito amado(a)')
