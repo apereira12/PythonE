ef368b5b550c863e280968c3ea209b0cbe0d2008
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:46:34 -0300

    Delete 002.py

--- 002.py
+++
@@ -1,7 +0,0 @@
-#Escreva um programa que leia, o nome, idade, e cidade de nascimento e retorne para o usuário
-
-nomecompleto = input('Digite seu nome: ')
-idade= input('Digite sua idade: ')
-cidade = input('Digite sua cidade natal: ')
-
-print(f'Meu nome é {nomecompleto}, tenho {idade} anos e nasci em {cidade}')
