9f399d7420467a7033a7be10ff3992c8ae938a41
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:46:56 -0300

    Delete 005.py

--- 005.py
+++
@@ -1,5 +0,0 @@
-#Escreva um programa que converta real para o Franco Congolês
-Real = float(input('Favor digitar o valor em reais que você quer converter:'))
-Franco =  float(500.86*Real)
-print(f'R${Real:.2f} , equivalem a {Franco :.2f} Francos Congoleses')
-
