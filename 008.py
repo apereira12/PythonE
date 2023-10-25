558ebec1232120cb2d0757de27dd9b96e790998c
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:47:56 -0300

    Delete 008.py

--- 008.py
+++
@@ -1,5 +0,0 @@
-#Crie um algoritmo que leia um salário e simule um reajuste positivo de 60%.
-salario = float(input('Informe o salário: '))
-aumento = salario*1.60
-print(f'O salário de {salario} com o reajuste de 60% será de : {aumento}')
-
