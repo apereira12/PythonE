45adde9c5518fdc404d1f14061306522874d720e
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:46:46 -0300

    Delete 003.py

--- 003.py
+++
@@ -1,6 +0,0 @@
-#Escreva um programa que leia o nome, e o sobrenome, CONCATENE em uma nova variável nome completo, e retorne para o usuário
-Nome = input('Digite seu nome: ')
-sobrenome= input('Digite seu sobrenome: ')
-nomecompleto = Nome+' ' + sobrenome
-
-print(f'Seu nome é: {nomecompleto}')
