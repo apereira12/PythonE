6696d1ce0234ffc2dee7636d1776b28314db6dcd
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:08:23 -0300

    extra009.py

--- extra009.py
+++ extra009.py
@@ -2,12 +2,15 @@
 #Simule um caixa eletrônico. Primeiro pergunte ao usuário qual valor ele quer retirar, depois dê o dinheiro com o menor número possível de notas.
 
 ValorSolicitado = int(input('Informe o valor que será retirado:'))
-Notas=[2,5,20,50,100,200]
+Notas = [200, 100, 50, 20, 5, 2]
 
 def menorquantidade():
+    sobra = ValorSolicitado
     for i in Notas:
-        divisao=ValorSolicitado/i
-        print(f'Nota de {i}: {divisao}')
+        if sobra >= i:
+            quantidade = sobra//i
+            sobra=sobra%i
+            print(f'Quantidade de notas {i} é: {quantidade}')
 
 menorquantidade()
 
