f588055d133d868dc137ffcf88d18a7fbfb8b0cc
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:06:54 -0300

    extra011.py

--- extra011.py
+++ extra011.py
@@ -1,3 +1,16 @@
 # Calculadora de Distância
 #Solicite ao usuário as coordenadas de dois pontos (x1, y1, x2, y2) e calcule a distância entre eles.
 
+#formula: ((xb-xa)**2+(yb-ya)**2)**0,5
+
+
+def formula(x1,x2,y1,y2):
+    distancia = ((x2-x1)**2+(y2-y1)**2)**0.5
+    print (f'a distância entre os dois pontos é de aproximadamente:{distancia:.2f}')
+
+x1 = float(input('Favor informar o ponto x1:'))
+y1 = float(input('Favor informar o ponto y1:'))
+x2 = float(input('Favor informar o ponto x2:'))
+y2 = float(input('Favor informar o ponto y2:'))
+
+formula(x1,x2,y1,y2)
