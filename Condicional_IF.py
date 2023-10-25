9acd4a58092c6501ec4eaff9b53b6229b591999c
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 01:08:18 -0300

    Delete Condicional_IF.py

--- Condicional_IF.py
+++
@@ -1,9 +0,0 @@
-
-
-altura = float(input('Digite sua altura: '))
-idade =int(input('Digite sua idade:'))
-
-if altura < 1.7:
-    print('Você não pode brincar')
-else:
-    print('Você pode brincar')
