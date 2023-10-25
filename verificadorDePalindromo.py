8af2a4fecb8e376acf3ae7e06c3f0cd82a235173
Alisson Henrique Pereira <103470596+apereira12@users.noreply.github.com>
2023-10-25 00:53:56 -0300

    verificadorDePalindromo.py

--- verificadorDePalindromo.py
+++ verificadorDePalindromo.py
@@ -3,12 +3,6 @@
 print('-------- Programa de verificação palíndromo --------')
 frase = input('Escreva aqui sua frase:')
 
-'''for i in range(len(frase)//2):
-    if frase[i] != frase[-i - 1]:
-        print('Não é um palíndromo')
-    else:
-        print('É um palíndromo')
-'''
 compatibilidade = 0
 for i in range(0,len(frase)):
 
